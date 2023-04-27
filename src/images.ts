8b2dfd48ffe010a073845c78ce6daace5fd1e8b3
DMSDESIGN001 <131871203+DMSDESIGN001@users.noreply.github.com>
2023-04-27 18:32:04 +0200

    Image delete

--- src/images.ts
+++ src/images.ts
@@ -8,20 +8,7 @@ export const imagesArray: GridImage[] = [
 		project: "Project Lier",
 		category: "interieur",
 	},
-    {
-		src: "images/lier/2.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/3.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
+    
 
     {
 		src: "images/mechelen/1.jpg",
@@ -80,145 +67,14 @@ export const imagesArray: GridImage[] = [
 		category: "interieur",
 	},
     {
-		src: "images/lier/6.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "ruwbouw",
-	},
-    {
-		src: "images/lier/7.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/8.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/9.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/10.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/11.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/12.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/13.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/14.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/15.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/16.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/17.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
 		src: "images/lier/18.jpg",
 		title: "",
 		description: "",
 		project: "Lier",
 		category: "interieur",
 	},
-    {
-		src: "images/lier/19.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/20.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/21.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/22.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/23.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/24.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
-    {
-		src: "images/lier/25.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
+   
+    
 
     {
 		src: "images/mechelen/4.jpg",
@@ -227,13 +83,7 @@ export const imagesArray: GridImage[] = [
 		project: "Mechelen",
 		category: "interieur",
 	},
-    {
-		src: "images/mechelen/5.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+  
     {
 		src: "images/mechelen/6.jpg",
 		title: "",
@@ -255,13 +105,7 @@ export const imagesArray: GridImage[] = [
 		project: "Mechelen",
 		category: "interieur",
 	},
-    {
-		src: "images/mechelen/9.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+
     {
 		src: "images/mechelen/10.jpg",
 		title: "",
@@ -276,13 +120,7 @@ export const imagesArray: GridImage[] = [
 		project: "Mechelen",
 		category: "interieur",
 	},
-    {
-		src: "images/mechelen/12.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+  
     {
 		src: "images/mechelen/13.jpg",
 		title: "",
@@ -290,20 +128,7 @@ export const imagesArray: GridImage[] = [
 		project: "Mechelen",
 		category: "interieur",
 	},
-    {
-		src: "images/mechelen/14.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
-    {
-		src: "images/mechelen/15.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+    
     {
 		src: "images/zemst/4.jpg",
 		title: "",
@@ -311,41 +136,7 @@ export const imagesArray: GridImage[] = [
 		project: "Zemst",
 		category: "ruwbouw",
 	},
-    {
-		src: "images/zemst/5.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "ruwbouw",
-	},
-    {
-		src: "images/zemst/6.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
-    {
-		src: "images/zemst/7.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
-    {
-		src: "images/zemst/8.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
-    {
-		src: "images/zemst/9.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
+   
     {
 		src: "images/zemst/10.jpg",
 		title: "",
@@ -353,20 +144,7 @@ export const imagesArray: GridImage[] = [
 		project: "Zemst",
 		category: "ruwbouw",
 	},
-    {
-		src: "images/zemst/11.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
-    {
-		src: "images/zemst/12.jpg",
-		title: "",
-		description: "",
-		project: "Zemst",
-		category: "interieur",
-	},
+   
     {
 		src: "images/zemst/13.jpg",
 		title: "",
