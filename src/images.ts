6179494ba8195666c0fdb3c44037fa76fff8026d
DMSDESIGN001 <131871203+DMSDESIGN001@users.noreply.github.com>
2023-04-27 19:06:01 +0200

    Volgorde foto's

--- src/images.ts
+++ src/images.ts
@@ -3,9 +3,9 @@ import type { GridImage } from './types'
 export const imagesArray: GridImage[] = [
 	{
 		src: "images/lier/1.jpg",
-		title: "Lorem ipsum dolor sit amet.",
-		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis odio vitae arcu cursus mollis nec vel odio. Curabitur vestibulum quam vitae consectetur rhoncus.",
-		project: "Project Lier",
+		title: "",
+		description: "",
+		project: "",
 		category: "interieur",
 	},
     
@@ -14,75 +14,105 @@ export const imagesArray: GridImage[] = [
 		src: "images/mechelen/1.jpg",
 		title: "",
 		description: "",
-		project: "Mechelen",
+		project: "",
 		category: "interieur",
 	},
+	{
+		src: "images/zemst/1.jpg",
+		title: "",
+		description: "",
+		project: "",
+		category: "interieur",
+	},
+
+	{
+		src: "images/mechelen/8.jpg",
+		title: "",
+		description: "",
+		project: "",
+		category: "interieur",
+	},
+
+	{
+		src: "images/lier/5.jpg",
+		title: "",
+		description: "",
+		project: "",
+		category: "interieur",
+	},
+
+	  {
+		src: "images/mechelen/7.jpg",
+		title: "",
+		description: "",
+		project: "",
+		category: "interieur",
+	},
+
+	{
+		src: "images/mechelen/10.jpg",
+		title: "",
+		description: "",
+		project: "",
+		category: "ruwbouw",
+	},
+
     {
 		src: "images/mechelen/2.jpg",
 		title: "",
 		description: "",
-		project: "Mechelen",
+		project: "",
 		category: "interieur",
 	},
     {
 		src: "images/mechelen/3.jpg",
 		title: "",
 		description: "",
-		project: "Mechelen",
+		project: "",
 		category: "ruwbouw",
 	},
-    {
-		src: "images/zemst/1.jpg",
+
+	{
+		src: "images/mechelen/4.jpg",
 		title: "",
 		description: "",
-		project: "Zemst",
+		project: "",
 		category: "interieur",
 	},
+    
     {
 		src: "images/zemst/2.jpg",
 		title: "",
 		description: "",
-		project: "Zemst",
+		project: "",
 		category: "interieur",
 	},
     {
 		src: "images/zemst/3.jpg",
 		title: "",
 		description: "",
-		project: "Zemst",
+		project: "",
 		category: "interieur",
 	},
     {
 		src: "images/lier/4.jpg",
 		title: "",
 		description: "",
-		project: "Lier",
+		project: "",
 		category: "interieur",
 	},
-    {
-		src: "images/lier/5.jpg",
-		title: "",
-		description: "",
-		project: "Lier",
-		category: "interieur",
-	},
+   
     {
 		src: "images/lier/18.jpg",
 		title: "",
 		description: "",
-		project: "Lier",
+		project: "",
 		category: "interieur",
 	},
    
     
 
-    {
-		src: "images/mechelen/4.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+    
   
     {
 		src: "images/mechelen/6.jpg",
@@ -91,28 +121,10 @@ export const imagesArray: GridImage[] = [
 		project: "Mechelen",
 		category: "interieur",
 	},
-    {
-		src: "images/mechelen/7.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
-    {
-		src: "images/mechelen/8.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "interieur",
-	},
+  
+    
 
-    {
-		src: "images/mechelen/10.jpg",
-		title: "",
-		description: "",
-		project: "Mechelen",
-		category: "ruwbouw",
-	},
+ 
     {
 		src: "images/mechelen/11.jpg",
 		title: "",
